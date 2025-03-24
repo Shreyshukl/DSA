4c906f3eab8dc5df69a4bfe8736860098be1a703
Shreyshukl <158249360+Shreyshukl@users.noreply.github.com>
2025-03-24 17:19:47 +0530

    Update 1502. Can Make Arithmetic Progression From Sequence.cpp

--- Maths/1502. Can Make Arithmetic Progression From Sequence.cpp
+++ Maths/1502. Can Make Arithmetic Progression From Sequence.cpp
@@ -2,6 +2,24 @@ class Solution {
 public:
     bool canMakeArithmeticProgression(vector<int>& arr) {
         int n = arr.size();
+        sort(arr.begin() , arr.end());
+        int diff = arr[1] - arr[0];
+        for(int i = 2 ; i<n ; i++)
+        {
+            
+            if(arr[i]-diff!=arr[i-1])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+class Solution {
+public:
+    bool canMakeArithmeticProgression(vector<int>& arr) {
+        int n = arr.size();
         unordered_set<int>st(arr.begin() , arr.end());
         int mini = *min_element(arr.begin() , arr.end());
         int maxi = *max_element(arr.begin() , arr.end());
