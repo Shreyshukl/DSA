7a899f4204dc85479104940b3c4da869e207c6e7
Shreyshukl <158249360+Shreyshukl@users.noreply.github.com>
2025-03-29 00:04:13 +0530

    Update 64. Minimum Path Sum.cpp

--- DP/64. Minimum Path Sum.cpp
+++ DP/64. Minimum Path Sum.cpp
@@ -1,5 +1,33 @@
 class Solution {
 public:
+    int minPathSum(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        int left = 0 ;
+        int down = 0;
+        for(int i=1 ; i<m ; i++)
+        {
+            grid[i][0]+=grid[i-1][0];
+        }
+
+         for(int j=1 ; j<n ; j++)
+        {
+            grid[0][j]+=grid[0][j-1];
+        }
+
+       for(int i = 1 ; i<m ; i++)
+       {
+        for(int j = 1 ; j<n ; j++)
+        {
+           grid[i][j]+= min(grid[i][j-1],grid[i-1][j]);
+        }
+       }
+       return grid[m-1][n-1];
+    }
+};
+
+class Solution {
+public:
     int dp[201][201];
     int solve(int i , int j , int m , int n , vector<vector<int>>& grid)
     {
