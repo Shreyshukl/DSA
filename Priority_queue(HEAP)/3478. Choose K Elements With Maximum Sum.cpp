3c654cc444cc473d905a5742c11a751d1574ea6a
Shreyshukl <158249360+Shreyshukl@users.noreply.github.com>
2025-04-03 13:03:19 +0530

    Update 3478. Choose K Elements With Maximum Sum.cpp

--- Priority_queue(HEAP)/3478. Choose K Elements With Maximum Sum.cpp
+++ Priority_queue(HEAP)/3478. Choose K Elements With Maximum Sum.cpp
@@ -1,3 +1,47 @@
+class Solution {
+public:
+    vector<long long> findMaxSum(vector<int>& nums1, vector<int>& nums2, int k) {
+        int n = nums1.size();
+
+        vector<vector<long long>>vec(n);
+
+        for(int i = 0 ; i<n ;i++)
+        {
+           vec[i]={nums1[i],i,nums2[i]};
+        }
+
+        vector<long long>result(n,0);
+        sort(vec.begin() ,vec.end());
+        long long sum = 0;
+        priority_queue<int , vector<int> , greater<int>>pq; //minheap
+        for(int i = 0 ; i< n ; i++)
+        {
+            if(i > 0 && vec[i-1][0] == vec[i][0])
+            {
+                long long ans = result[vec[i-1][1]];
+                result[vec[i][1]]=ans;
+            }
+            else
+            {
+                result[vec[i][1]] = sum;
+            }
+
+            sum+=vec[i][2];
+            pq.push(vec[i][2]);
+            if(pq.size()>k)
+            {
+                sum-=pq.top();
+                pq.pop();
+            }
+           
+        }
+        return result;
+    }
+};
+
+
+
+
 // Brute force
 class Solution {
 public:
