99e3709dd176e80f6136a0e23ed30f645d5762f6
Shreyshukl <158249360+Shreyshukl@users.noreply.github.com>
2025-08-06 02:37:36 +0530

    Update 1498. Number of Subsequences That Satisfy the Given Sum Condition.cpp

--- Two_Pointer/1498. Number of Subsequences That Satisfy the Given Sum Condition.cpp
+++ Two_Pointer/1498. Number of Subsequences That Satisfy the Given Sum Condition.cpp
@@ -1,5 +1,37 @@
 class Solution {
 public:
+    const int MOD = 1e9+7;
+    int numSubseq(vector<int>& nums, int target) {
+        int n = nums.size();
+        sort(nums.begin() , nums.end());
+        int l = 0 , r = n-1;
+        int count = 0;
+
+        vector<int>power(n,1);
+        for(int i = 1 ; i<n ;i++)
+        {
+            power[i] = (power[i-1]*2)%MOD;
+        }
+        while(l<=r)
+        {
+            if(nums[l]+nums[r]<=target)
+            {
+                count=(count+power[r-l])%MOD; //left ko fix krke
+                l++; 
+            }
+            else
+            {
+                r--;
+            }
+        }
+        return count;
+    }
+};
+
+
+
+class Solution {
+public:
     int count = 0;
     const int MOD = 1e9+7;
     void solve(int i , vector<int>& nums , vector<int>& path ,int target)
